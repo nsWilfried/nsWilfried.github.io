2d3cdd83b13b202d7b8e807b01180cb42b2c5fc8
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-20 17:36:50 +0000

    Add some pages

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template >
+    <!-- // header -->
+        <div class="index__header">
+            <span>je suis le header</span>
+        </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="css">
+    .index__header {
+        width: 1320px;
+        height: auto;
+        background-color: white;
+    }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,30 @@
-<template lang="">
-    <div>
-        Hello tout le monde
+<template>
+    <div class="index w-full flex justify-center items-center h-screen">
+
+        <!-- header -->
+        <Header />
+
     </div>
 </template>
 <script>
-    import client from "../backend/pocketbase"
+import client from "../backend/pocketbase"
+import Header from "@/components/Header.vue";
 export default {
+    components: { Header },
     data() {
         return {
-            
+
         }
     },
-    async created(){
-        await client.admins.authViaEmail(import.meta.env.VITE_POCKETBASE_ADMIN_EMAIL, import.meta.env.VITE_POCKETBASE_ADMIN_PASSWORD);
-        console.log("je suis le client et je répond peut être bien, qui sait ", client.records.getList("tests"))
+    async created() {
+        // await client.admins.authViaEmail(import.meta.env.VITE_POCKETBASE_ADMIN_EMAIL, import.meta.env.VITE_POCKETBASE_ADMIN_PASSWORD);
+        // console.log("je suis le client et je répond peut être bien, qui sait ", client.records.getList("tests"))
     }
 }
 </script>
 <style lang="css">
-    
+.index {
+    background-color: rgb(38, 48, 60);
+    height: 100vh;
+}
 </style>
