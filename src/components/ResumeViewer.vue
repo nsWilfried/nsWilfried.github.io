57340af3a07297477aa411a7522c5346f6ee3bd7
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-26 15:29:09 +0000

    Intregate resume

--- src/components/ResumeViewer.vue
+++ src/components/ResumeViewer.vue
@@ -1,9 +1,11 @@
 <template>
     <!-- resume viewer -->
-    <div class="w-full flex justify-center items-center">
+    <div class="w-full h-screen">
+        <embed width="100%" height="100%" src="../public/Resume-Template.pdf"  type='application/pdf'/>
+
+    </div>
         <!-- <pdf src="https://cdn.mozilla.net/pdfjs/tracemonkey.pdf"></pdf> -->
         <!-- <vue-pdf-embed class="shadow-md" source="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" /> -->
-    </div>
 </template>
 <script>
 export default {
