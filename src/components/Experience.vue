42c51890d796fe5afd9db4776573d59c116f4cd3
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-09-29 17:31:52 +0000

    Update Experience.vue

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -15,20 +15,22 @@
                         <div class="py-8 flex flex-wrap md:flex-nowrap">
                             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                                 <span class="font-medium title-font text-gray-700">Freelance Front-End Developer</span>
-                                <span class="mt-1 text-gray-500 text-sm secondary-font pt-3">12 Jun 2019</span>
+                                <span class="mt-1 text-gray-500 text-sm secondary-font pt-3">Août 2022</span>
                             </div>
                             <div class="md:flex-grow">
                                 <h2 class="text-2xl font-medium underline title-font mb-2 text-red-400 ">@ESMC GIE</h2>
-                                <p class="leading-relaxed secondary-font pt-5">Glossier echo park pug, church-key sartorial biodiesel
-                                    vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag
-                                    selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland
-                                    craft beer.</p>
+                                <p class="leading-relaxed secondary-font pt-5">
+                                        <li>Consommation d'Api Restfull en collaboration avec une équipe backend développant des Microservices SpringBoot</li>
+                                        <li>Développement d'un système de Gestion d'utilisateurs dans le Dashboard, cette solution notamment permet à présent de gérer tous les droits utilisateurs</li>
+                                        <li>Prise en main parfaite et rapide du template de développement du back office</li>
+                                        <li>Chargé de designer l'interface d'administration afin de le rendre plus user-friendly</li>
+                                </p>
                             </div>
                         </div>
                         <div class="py-8 flex flex-wrap md:flex-nowrap">
                             <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                                 <span class="font-medium title-font text-gray-700">Freelance FullStack Developer</span>
-                                <span class="mt-1 text-gray-500 text-sm secondary-font pt-3">12 Jun 2019</span>
+                                <span class="mt-1 text-gray-500 text-sm secondary-font pt-3">Avril 2020</span>
                             </div>
                             <div class="md:flex-grow">
                                 <h2 class="text-2xl font-medium underline title-font mb-2 text-red-400 ">@Collège Père
