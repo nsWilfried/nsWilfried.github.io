8b53f064253c1775900d0859360231e894edc57f
Wilfried N'SOUKPOE <wilfriednsoukpoe1@gmail.com>
2022-10-14 10:44:58 +0000

    Portfolio Production v1.0.0

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="overlay shadow-sm">
-        <Logo/>
+        <Logo />
     </div>
     <!-- portfolio page -->
     <div class="index w-full  h-auto">
@@ -9,23 +9,25 @@
         <Header />
 
         <!-- about -->
-        <About/>
+        <About />
 
         <!-- experiences -->
-        <Experience/>
+        <Experience />
+
+
+
+        <!-- Project -->
+        <Project />
 
         <!-- education -->
         <Education />
 
-        <!-- Project -->
-        <Project/>
-
         <!-- contact -->
-        <Contact/>
+        <Contact />
     </div>
 </template>
 <script>
-    import ScrollTrigger from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import client from "../backend/pocketbase"
 import Header from "@/components/Header.vue";
 import About from "@/components/About.vue";
@@ -48,25 +50,25 @@ export default {
     async created() {
         // await client.admins.authViaEmail(import.meta.env.VITE_POCKETBASE_ADMIN_EMAIL, import.meta.env.VITE_POCKETBASE_ADMIN_PASSWORD);
         // console.log("je suis le client et je répond peut être bien, qui sait ", client.records.getList("tests"))
-    }, 
+    },
     mounted() {
-        const tl  = gsap.timeline()
+        const tl = gsap.timeline()
         tl.to(".overlay", {
-            duration: 2, 
-            translateY: "-100%",  
+            duration: 2,
+            translateY: "-100%",
             ease: "Expo.easeInOut"
         })
-        .from(".navbar__logo", {
-            opacity: 0,
-                y: 20,
-                duration: 0.2,
-                ease:"Expo.easeInOut",
-        })
-        .from("nav li", {
+            .from(".navbar__logo", {
                 opacity: 0,
                 y: 20,
                 duration: 0.2,
-                ease:"Expo.easeInOut",
+                ease: "Expo.easeInOut",
+            })
+            .from("nav li", {
+                opacity: 0,
+                y: 20,
+                duration: 0.2,
+                ease: "Expo.easeInOut",
                 stagger: 0.1
                 // delay: el.dataset.index * 0.13
             })
@@ -75,7 +77,7 @@ export default {
                 opacity: 0,
                 y: 20,
                 duration: 0.2,
-                ease:"Expo.easeInOut",
+                ease: "Expo.easeInOut",
                 // delay: el.dataset.index * 0.13
             })
 
@@ -83,128 +85,131 @@ export default {
                 opacity: 0,
                 y: 20,
                 duration: 0.2,
-                ease:"Expo.easeInOut",
+                ease: "Expo.easeInOut",
                 stagger: 0.2
             })
-           gsap.from(".about", {
-            duration: 1.5, 
-            y: 20, 
-            opacity: 0, 
+        gsap.from(".about", {
+            duration: 1.5,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".about__title"
-           })
+        })
 
-           gsap.from(".experience__title", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        gsap.from(".experience__title", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".experience__title"
-           })
-           gsap.from(".job", {
-            duration: 2, 
-            y: 20, 
-            opacity: 0, 
+        })
+        gsap.from(".job", {
+            duration: 2,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
-            stagger: 0.3, 
+            stagger: 0.3,
             scrollTrigger: ".experience__title"
 
-           })
-           gsap.from(".education__title", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        })
+        gsap.from(".education__title", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".education__title"
-           })
-           gsap.from(".school", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        })
+        gsap.from(".school", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
-            stagger: 0.2, 
+            stagger: 0.2,
             scrollTrigger: ".education__title"
 
-           })
-           gsap.from(".projects__title", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        })
+        gsap.from(".projects__title", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".projects__title"
-           })
-           gsap.from(".project", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        })
+        gsap.from(".project", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
-            stagger: 0.2, 
+            stagger: 0.2,
             scrollTrigger: ".projects__title"
 
-           })
+        })
 
-           gsap.from(".contact__title , .contact__description", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        gsap.from(".contact__title , .contact__description", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".contact__title"
-           })
-           
-           gsap.from(".contact__content", {
-            duration: 1, 
-            y: 20, 
-            opacity: 0, 
+        })
+
+        gsap.from(".contact__content", {
+            duration: 1,
+            y: 20,
+            opacity: 0,
             ease: "Expo.easeInOut",
             scrollTrigger: ".contact__title"
-           })
-          
+        })
+
     },
 }
 </script>
 <style lang="scss" >
-    
-    @import "../assets/scss/variables";
-    
-    .overlay {
-        width: 100%;
-        height: 100vh ;
-        background-color: white ;
-        position: absolute;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        z-index: 100000;
-        display: flex ;
-        justify-content: center;
-        align-items: center ;
+@import "../assets/scss/variables";
 
-    }
-html{
+.overlay {
+    width: 100%;
+    height: 100vh;
+    background-color: white;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 100000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+html {
     scroll-behavior: smooth;
 }
+
 .index {
     background-color: $ligth-bg;
     font-family: $primary-font;
 }
-.primary-color{
-    color:  #EC7063;
+
+.primary-color {
+    color: #EC7063;
 }
+
 body {
-  background-color: #ddd;
-  padding: 30px  ;
-  height: auto;
+    background-color: #ddd;
+    padding: 30px;
+    height: auto;
 
 }
 
 // laptop
-@media only screen and (min-width: 320px) and (max-width: 1320px){
+@media only screen and (min-width: 320px) and (max-width: 1320px) {
     body {
-  background-color: white;
-  padding: 0px   ;
-  height: auto;
+        background-color: white;
+        padding: 0px;
+        height: auto;
 
-}
+    }
 }
 </style>
